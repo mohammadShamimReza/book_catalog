521ea76ae59b8abf6bc5e67b9d1611bf8e0896d6
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-07-19 13:20:40 +0600

    [DONE] fixsignup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,70 +1,117 @@
-import { LoginForm } from '@/components/LoginForm';
-import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/technet-logo-white.png';
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Perform login or authentication logic here
+    console.log(formData);
+
+    // Reset the form
+    setFormData({
+      email: '',
+      password: '',
+    });
+  };
+
+  const isSubmitDisabled =
+    formData.email.trim() === '' || formData.password.trim() === '';
+
   return (
-    <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          to="/signup"
-          className={cn(
-            buttonVariants({ variant: 'ghost', size: 'sm' }),
-            'absolute right-4 top-4 md:right-8 md:top-8'
-          )}
-        >
-          Signup
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div
-            className="absolute inset-0 bg-cover"
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)',
-            }}
-          />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <Link to="/">
-              <img className="h-8" src={logo} alt="" />
-            </Link>{' '}
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2"></blockquote>
-          </div>
+    <div>
+      <nav className="flex items-center justify-between px-4 py-2 bg-gray-200">
+        <div>
+          <Link to="/" className="text-xl font-bold">
+            Home
+          </Link>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Login to your account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below
-              </p>
+        <div>
+          <Link
+            to="/signup"
+            className={cn(
+              'text-sm font-bold text-blue-500',
+              'ml-auto', // Move the button to the right corner
+              'w-min', // Set minimum width
+              'max-w-xs', // Set maximum width
+              'px-4', // Add horizontal padding
+              'py-2' // Add vertical padding
+            )}
+          >
+            Signup
+          </Link>
+        </div>
+      </nav>
+
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-full max-w-md">
+          <div className="mx-auto p-8 bg-white rounded-lg shadow-md">
+            <div className="flex justify-between mb-6">
+              <h1 className="text-2xl font-semibold text-center">Login</h1>
             </div>
-            <LoginForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
-              <Link
-                to="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link
-                to="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
+            <form className="mt-4" onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label htmlFor="email" className="text-sm font-bold">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="text-sm font-bold">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                    isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''
+                  }`}
+                  disabled={isSubmitDisabled}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -60,131 +60,149 @@ export default function Signup() {
     formData.password !== formData.confirmPassword;
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="w-full max-w-md">
-        <div className="mx-auto p-8 bg-white rounded-lg shadow-md">
-          <div className="flex justify-between mb-6">
-            <h1 className="text-2xl font-semibold text-center">
-              Create an account
-            </h1>
-            <Link to="/login" className={cn('text-sm font-bold text-blue-500')}>
-              Login
-            </Link>
-          </div>
-          <p className="text-sm text-left text-gray-600">
-            Enter your email below to create your account
-          </p>
-          <form className="mt-4" onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="firstName" className="text-sm font-bold">
-                First Name<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="lastName" className="text-sm font-bold">
-                Last Name<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="text-sm font-bold">
-                Email<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="text-sm font-bold">
-                Password<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="confirmPassword" className="text-sm font-bold">
-                Confirm Password<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-              {passwordError && (
-                <p className="text-red-500 text-xs mt-1">{passwordError}</p>
-              )}
-            </div>
-            <div className="mb-4">
-              <label htmlFor="address" className="text-sm font-bold">
-                Address<span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                id="address"
-                name="address"
-                value={formData.address}
-                onChange={handleChange}
-                required
-                className="w-full px-3 py-2 border rounded"
-              />
-            </div>
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
-                  isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''
-                }`}
-                disabled={isSubmitDisabled}
+    <div>
+      <nav className="flex items-center justify-between px-4 py-2 bg-gray-200">
+        <div>
+          <Link to="/" className="text-xl font-bold">
+            Home
+          </Link>
+        </div>
+        <div>
+          <Link to="/login" className={cn('text-sm font-bold text-blue-500')}>
+            Login
+          </Link>
+        </div>
+      </nav>
+
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-full max-w-md">
+          <div className="mx-auto p-8 bg-white rounded-lg shadow-md">
+            <div className="flex justify-between mb-6">
+              <h1 className="text-2xl font-semibold text-center">
+                Create an account
+              </h1>
+              <Link
+                to="/login"
+                className={cn('text-sm font-bold text-blue-500')}
               >
-                Submit
-              </button>
+                Login
+              </Link>
             </div>
-          </form>
-          <p className="mt-4 text-sm text-center text-gray-600">
-            By clicking continue, you agree to our{' '}
-            <Link to="/terms" className="underline hover:text-primary">
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link to="/privacy" className="underline hover:text-primary">
-              Privacy Policy
-            </Link>
-            .
-          </p>
+            <p className="text-sm text-left text-gray-600">
+              Enter your email below to create your account
+            </p>
+            <form className="mt-4" onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label htmlFor="firstName" className="text-sm font-bold">
+                  First Name<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="lastName" className="text-sm font-bold">
+                  Last Name<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="email" className="text-sm font-bold">
+                  Email<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="text-sm font-bold">
+                  Password<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="confirmPassword" className="text-sm font-bold">
+                  Confirm Password<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+                {passwordError && (
+                  <p className="text-red-500 text-xs mt-1">{passwordError}</p>
+                )}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="address" className="text-sm font-bold">
+                  Address<span className="text-red-500">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                  required
+                  className="w-full px-3 py-2 border rounded"
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                    isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''
+                  }`}
+                  disabled={isSubmitDisabled}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+            <p className="mt-4 text-sm text-center text-gray-600">
+              By clicking continue, you agree to our{' '}
+              <Link to="/terms" className="underline hover:text-primary">
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link to="/privacy" className="underline hover:text-primary">
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </div>
