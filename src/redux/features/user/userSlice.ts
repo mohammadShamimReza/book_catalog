06f324cfdebfc6ea1c87b43caab7c45799dcef7b
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-07-20 20:44:30 +0600

    [done] user reducer

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,14 +1,27 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-// interface ICredential {
-//   email: string;
-//   password: string;
-// }
+interface IUserState {
+  user: {
+    email: string | null;
+  };
+}
 
-// export const createUser = createAsyncThunk(
-//   'user/createUser',
-//   async ({ email, password }: ICredential) => {
-//     const data = await fetch()
-//     return data.user.email;
-//   }
-// );
+const initialState: IUserState = {
+  user: {
+    email: null,
+  },
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<string | null>) => {
+      state.user.email = action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from './api/apiSlice';
-// ...
+import useReducer from './features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
+    user: useReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
